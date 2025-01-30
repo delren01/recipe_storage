67f871efa61472847512bbbfeb77e0b7052f41c6
delren01 <delrensj@gmail.com>
2025-01-30 18:40:19 -0500

    Setting changed

--- recipe_manager/settings.py
+++ recipe_manager/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'recipe_manager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/data/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

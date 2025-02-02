797bb65032521e4feb2e241a5df3bca0e929c042
delren01 <delrensj@gmail.com>
2025-02-02 01:57:07 -0500

    changed url password

--- recipe_manager/settings.py
+++ recipe_manager/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'recipe_manager.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default="postgresql://postgres:Delren1201%21@db.lsbwvqqbdcsdhfwhbpau.supabase.co:5432/postgres"
+        default="postgresql://postgres:82vHcsjXGc47EMCW@db.lsbwvqqbdcsdhfwhbpau.supabase.co:5432/postgres"
     )
 }
 

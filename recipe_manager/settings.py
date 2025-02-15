1c77a709fca21d460d8f19cbebaae97c81c4671c
delren01 <delrensj@gmail.com>
2025-02-15 17:49:38 -0500

    updated templates in settings.py

--- recipe_manager/settings.py
+++ recipe_manager/settings.py
@@ -61,7 +61,10 @@ ROOT_URLCONF = 'recipe_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'recipes' / 'templates',
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

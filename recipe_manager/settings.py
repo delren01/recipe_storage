39394b570376ce25d6248faa11282252ebaab55f
delren01 <delrensj@gmail.com>
2025-02-15 22:13:04 -0500

    changed settings

--- recipe_manager/settings.py
+++ recipe_manager/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'recipe_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:8UEDY9TJevfI4dHD@db.lsbwvqqbdcsdhfwhbpau.supabase.co:5432/postgres')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:Gbxvu3zA9exQcEdL@db.lsbwvqqbdcsdhfwhbpau.supabase.co:5432/postgres')
 
 DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
